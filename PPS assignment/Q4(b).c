6928779f5fa2920f7683cf87f1097be5416932ce
SiddharthBhat06 <siddharthbhat99@gmail.com>
2024-12-02 14:23:27 +0530

    assignment push

--- PPS assignment/Q4(b).c
+++ PPS assignment/Q4(b).c
@@ -5,7 +5,7 @@ int main()
     int reg;
     char nam[100];
     printf("Enter student name :");
-    gets(nam);
+    scanf("%[^\n]", &nam);
     printf("Enter student registration number :");
     scanf("%d", &reg);
     printf("\n\n");
@@ -14,8 +14,8 @@ int main()
     
     char a[100],b[100]="";
     int i,len,j;
-    printf("Enter your string :");
-    gets(a);
+    printf("\nEnter your string :");
+    scanf(" %[^\n]", a);
     len=strlen(a);
     j=0;
     for(i=0;i<(len-1);i++){
